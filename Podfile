7e1ddb1451a5e77e0490074ca5b211e31b521b73
Jongui <joaogd53@gmail.com>
2017-12-02 18:36:42 -0500

    Initial commit

---
+++ Podfile
@@ -0,0 +1,15 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'MennoMaps' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  #use_frameworks!
+
+  # Pods for MennoMaps
+  pod 'GoogleMaps'
+  pod 'Google-Maps-iOS-Utils'
+  pod 'Firebase/Core'
+  pod 'Firebase/Database'
+end
---
+++ google_maps_bridge.h
@@ -0,0 +1,20 @@
+//
+//  google_maps_bridge.h
+//  GoogleMapsTest2
+//
+//  Created by João Dyck on 2017-12-02.
+//  Copyright © 2017 João Dyck. All rights reserved.
+//
+
+#ifndef google_maps_bridge_h
+#define google_maps_bridge_h
+
+#import "Google-Maps-iOS-Utils/GMUGeometryRenderer.h"
+#import "Google-Maps-iOS-Utils/GMUKMLParser.h"
+#import "Google-Maps-iOS-Utils/GMUGeoJSONParser.h"
+#import "Google-Maps-iOS-Utils/GMUDefaultClusterIconGenerator.h"
+#import "Google-Maps-iOS-Utils/GMUNonHierarchicalDistanceBasedAlgorithm.h"
+#import "Google-Maps-iOS-Utils/GMUDefaultClusterRenderer.h"
+#import "Google-Maps-iOS-Utils/GMUClusterManager.h"
+
+#endif /* google_maps_bridge_h */
